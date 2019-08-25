687ba291b840aa58fa4d0f724887db1048eed903
Duncan Doyle <Duncan.Doyle@gmail.com>
2019-08-25 19:44:59 +0200

    Added logging of chain to make it easier to inspect the implementation semantics of the shadow variable listener.

--- optaplanner-training-vehicle-routing-solution/src/main/java/org/optaplanner/training/vehiclerouting/app/VehicleRoutingApp.java
+++ optaplanner-training-vehicle-routing-solution/src/main/java/org/optaplanner/training/vehiclerouting/app/VehicleRoutingApp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,9 @@ import java.io.File;
 import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
 import org.optaplanner.persistence.xstream.impl.domain.solution.XStreamSolutionFileIO;
-import org.optaplanner.training.vehiclerouting.domain.VehicleRoutingSolution;
+import org.optaplanner.training.vehiclerouting.domain.Standstill;
+import org.optaplanner.training.vehiclerouting.domain.Vehicle;
+import org.optaplanner.training.vehiclerouting.domain.timewindowed.TimeWindowedCustomer;
 import org.optaplanner.training.vehiclerouting.domain.timewindowed.TimeWindowedVehicleRoutingSolution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,57 +37,51 @@ public class VehicleRoutingApp {
 
     public static final String DATA_DIR_NAME = "vehiclerouting";
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
         SolverFactory<TimeWindowedVehicleRoutingSolution> solverFactory = SolverFactory.createFromXmlResource(SOLVER_CONFIG);
         
         File inputSolutionFileOne = new File("data/vehiclerouting/unsolved/cvrptw-100customers-A.xml");
-        
         XStreamSolutionFileIO fileIO = new XStreamSolutionFileIO<>(TimeWindowedVehicleRoutingSolution.class);
-        
         TimeWindowedVehicleRoutingSolution problemOne = (TimeWindowedVehicleRoutingSolution) fileIO.read(inputSolutionFileOne);
         
         Solver<TimeWindowedVehicleRoutingSolution> solver = solverFactory.buildSolver();
-                
+        
         TimeWindowedVehicleRoutingSolution solutionOne = solver.solve(problemOne);
         
         String scoreExplanation = solver.explainBestScore();
         LOGGER.debug("Finished solving. Explaining the score:");
         LOGGER.debug(scoreExplanation);
         
+        logFirstChain(solutionOne);
+
         File outputSolutionFileOne = new File("data/vehiclerouting/solved/cvrptw-100customers-A.xml");
         fileIO.write(solutionOne, outputSolutionFileOne);
     }
 
-    /*
-    public VehicleRoutingApp() {
-        super("Vehicle routing",
-                "Official competition name: Capacitated vehicle routing problem (CVRP), " +
-                        "optionally with time windows (CVRPTW)\n\n" +
-                        "Pick up all items of all customers with a few vehicles.\n\n" +
-                        "Find the shortest route possible.\n" +
-                        "Do not overload the capacity of the vehicles.\n" +
-                        "Arrive within the time window of each customer.",
-                SOLVER_CONFIG, DATA_DIR_NAME,
-                VehicleRoutingPanel.LOGO_PATH);
+
+    private static void logFirstChain(TimeWindowedVehicleRoutingSolution solution) {
+        //Grab the first anchor.
+        Vehicle vehicle = solution.getVehicleList().stream().filter(v -> (getChainSize(v) >= 5)).findFirst().get();
+
+        StringBuilder chainStringBuilder = new StringBuilder().append("Chain of vehicle: " + vehicle.getId());
+        TimeWindowedCustomer nextCustomer = (TimeWindowedCustomer) vehicle.getNextCustomer();
+        while (nextCustomer != null) {
+            chainStringBuilder.append("\n- Customer: ").append(nextCustomer.getLocation());
+            chainStringBuilder.append(",\t arrivalTime: ").append(nextCustomer.getArrivalTime());
+            chainStringBuilder.append(",\t dueTime: ").append(nextCustomer.getDueTime());
+            nextCustomer = nextCustomer.getNextCustomer();
+        }
+
+        LOGGER.debug(chainStringBuilder.toString());
     }
 
-    @Override
-    protected VehicleRoutingPanel createSolutionPanel() {
-        return new VehicleRoutingPanel();
+    private static int getChainSize(Vehicle vehicle) {
+        int size = 0;
+        Standstill standstill = (Standstill) vehicle.getNextCustomer();
+        while(standstill != null) {
+            standstill = (Standstill) standstill.getNextCustomer();
+            size++;
+        }
+        return size;
     }
-
-    @Override
-    public SolutionFileIO<VehicleRoutingSolution> createSolutionFileIO() {
-        return new XStreamSolutionFileIO<>(VehicleRoutingSolution.class);
-    }
-
-    @Override
-    protected AbstractSolutionImporter[] createSolutionImporters() {
-        return new AbstractSolutionImporter[]{
-                new VehicleRoutingImporter()
-        };
-    }
-    */
-
 }
